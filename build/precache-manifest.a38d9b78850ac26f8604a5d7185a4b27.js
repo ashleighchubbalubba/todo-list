1ebb33a510711df06471e5ceb0475c5932836f9d
TOTO <toto@TOTOs-MacBook-Pro-2.local>
2018-10-20 17:25:11 -0400

    first commit

---
+++ build/precache-manifest.a38d9b78850ac26f8604a5d7185a4b27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8099b5f3eb65852ccea1",
+    "url": "/static/js/main.8099b5f3.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "8099b5f3eb65852ccea1",
+    "url": "/static/css/main.60167f14.chunk.css"
+  },
+  {
+    "revision": "a25bb7ffcad64d3a559428449fd1a763",
+    "url": "/index.html"
+  }
+];
